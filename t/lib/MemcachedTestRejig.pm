2de4e082e0f6217ca51a1de2718141965b79bfa1
arunangshu-chatterjee <arunangshu.kiit@gmail.com>
2018-12-02 22:21:21 -0800

    Update MemcachedTestRejig.pm
    updates with rejig_mem_gets_is for cas-rejig,t

--- t/lib/MemcachedTestRejig.pm
+++ t/lib/MemcachedTestRejig.pm
@@ -5,7 +5,7 @@ use Carp qw(croak);
 use vars qw(@EXPORT);
 use MemcachedTest;
 
-@EXPORT = qw(rejig_mem_get_is mem_lease_stats all_in_list);
+@EXPORT = qw(rejig_mem_get_is mem_lease_stats all_in_list rejig_mem_gets_is);
 
 # rejig version of mem_get_is.
 sub rejig_mem_get_is {
@@ -64,4 +64,35 @@ sub all_in_list {
         $i += 1;
     }
     return 1;
+
+sub rejig_mem_gets_is {
+    # works on single-line values only.  no newlines in value.
+    my ($sock_opts, $config_id, $fragment_num, $identifier, $key, $val, $msg) = @_;
+    my $opts = ref $sock_opts eq "HASH" ? $sock_opts : {};
+    my $sock = ref $sock_opts eq "HASH" ? $opts->{sock} : $sock_opts;
+
+    my $expect_flags = $opts->{flags} || 0;
+    my $dval = defined $val ? "'$val'" : "<undef>";
+    $msg ||= "$key == $dval";
+
+    print $sock "rj $config_id $fragment_num gets $key\r\n";
+    if (! defined $val) {
+        my $line = scalar <$sock>;
+        if ($line =~ /^VALUE/) {
+            $line .= scalar(<$sock>) . scalar(<$sock>);
+        }
+        Test::More::is($line, "END\r\n", $msg);
+    } else {
+        my $len = length($val);
+        my $body = scalar(<$sock>);
+        my $expected = "VALUE $key $expect_flags $len $identifier $config_id\r\n$val\r\nEND\r\n";
+        if (!$body || $body =~ /^END/) {
+            Test::More::is($body, $expected, $msg);
+            return;
+        }
+        $body .= scalar(<$sock>) . scalar(<$sock>);
+        Test::More::is($body, $expected, $msg);
+    }
+}
+
 }
